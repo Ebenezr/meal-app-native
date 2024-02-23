bbddda85619339f75090f8747ba8c26d82fcfb45
#ebenezr <ebenezarbukosia@gmail.com>
2024-02-23 12:45:57 +0300

    chore: setup context actions

--- store/favorite-context.js
+++ store/favorite-context.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const FavoriteContext = createContext({
   favorites: [],
@@ -10,8 +10,34 @@ const FavoriteContext = createContext({
 export { FavoriteContext };
 
 const FavoriteContextProvider = ({ children }) => {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const addFavoriteHandler = (favorite) => {
+    setUserFavorites((currentFavorites) => {
+      return currentFavorites.concat(favorite);
+    });
+  };
+  const removeFavoriteHandler = (favoriteId) => {
+    setUserFavorites((currentFavorites) => {
+      return currentFavorites.filter((favorite) => favorite.id !== favoriteId);
+    });
+  };
+
+  const itemIsFavoriteHandler = (favoriteId) => {
+    return userFavorites.some((favorite) => favorite.id === favoriteId);
+  };
+
+  const context = {
+    favorites: userFavorites,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    isFavorite: itemIsFavoriteHandler,
+  };
+
   return (
-    <FavoriteContext.Provider value={{}}>{children}</FavoriteContext.Provider>
+    <FavoriteContext.Provider value={context}>
+      {children}
+    </FavoriteContext.Provider>
   );
 };
 
